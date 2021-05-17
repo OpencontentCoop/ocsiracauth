ed56903b159b99f01f6957664227cbe981597cea
Luca Realdi <lr@opencontent.it>
2021-05-17 15:31:40 +0200

    Introduce la compatibilità con openlogin-oauth

--- classes/OCOpenLoginAuthUserHandler.php
+++ classes/OCOpenLoginAuthUserHandler.php
@@ -5,6 +5,13 @@ class OCOpenLoginAuthUserHandler extends OCSiracAuthUserHandler
 {
     public function __construct()
     {
+        if (isset($_SERVER['HTTP_X_FORWARDED_USER'])
+            && base64_encode(base64_decode($_SERVER['HTTP_X_FORWARDED_USER'])) == $_SERVER['HTTP_X_FORWARDED_USER']){
+            $userData = (array)json_decode(base64_decode($_SERVER['HTTP_X_FORWARDED_USER']), true);
+            foreach ($userData as $key => $value){
+                $_SERVER['HTTP_X_FORWARDED_USER_' . strtoupper($key)] = $value;
+            }
+        }
         parent::__construct();
 
         $this->mappedVars['UserLogin'] = str_replace('TINIT-', '', $this->mappedVars['UserLogin']);
