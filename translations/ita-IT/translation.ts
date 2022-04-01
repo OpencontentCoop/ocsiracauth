6e43477bd0b233360cd7a9ebbe3c661e6dac6ee5
Luca Realdi <lr@opencontent.it>
2022-04-01 17:20:52 +0200

    Corregge un bug sui file delle traduzioni

--- translations/ita-IT/translation.ts
+++ translations/ita-IT/translation.ts
@@ -2,13 +2,14 @@
 <!DOCTYPE TS>
 <TS version="2.0">
     <context>
-    <name>sirac</name>
-    <message>
-        <source>It is not possible to change the password for your user from this application, as an authentication has been performed through an external provider</source>
-        <translation>Non è possibile cambiare la password per il tuo utente da questa applicazione, in quanto è stato eseguito un'autenticazione attraverso un provider esterno</translation>
-    </message>
-    <message>
-        <source>Back to site</source>
-        <translation>Torna al sito</translation>
-    </message>
+        <name>sirac</name>
+        <message>
+            <source>It is not possible to change the password for your user from this application, as an authentication has been performed through an external provider</source>
+            <translation>Non è possibile cambiare la password per il tuo utente da questa applicazione, in quanto è stata eseguita un'autenticazione attraverso un provider esterno</translation>
+        </message>
+        <message>
+            <source>Back to site</source>
+            <translation>Torna al sito</translation>
+        </message>
+    </context>
 </TS>
--- translations/untranslated/translation.ts
+++ translations/untranslated/translation.ts
@@ -2,13 +2,14 @@
 <!DOCTYPE TS>
 <TS version="2.0">
     <context>
-    <name>sirac</name>
-    <message>
-        <source>It is not possible to change the password for your user from this application, as an authentication has been performed through an external provider</source>
-        <translation type="unfinished"/>
-    </message>
-    <message>
-        <source>Back to site</source>
-        <translation type="unfinished"/>
-    </message>
+        <name>sirac</name>
+        <message>
+            <source>It is not possible to change the password for your user from this application, as an authentication has been performed through an external provider</source>
+            <translation type="unfinished"/>
+        </message>
+        <message>
+            <source>Back to site</source>
+            <translation type="unfinished"/>
+        </message>
+    </context>
 </TS>
